1976d225f08a04f69dfce0eed89701df197564c3
ComeNahMan <xtremeeligon@gmail.com>
2021-11-01 21:13:01 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var status = true;
+var name = "Matt";
+var prompt = "Welcome "+ name + " Thank You for being fully vaccinated you can now enter!"
+
+var el = document.getElementById('end');
+
+el.textContent = prompt;
